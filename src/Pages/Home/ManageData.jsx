d2b0a5690ea04060750445ed333f767c117f49c6
hariss128 <harishameed128@gmail.com>
2024-12-20 13:04:32 +0500

    editmanagedata

--- src/Pages/Home/ManageData.jsx
+++ src/Pages/Home/ManageData.jsx
@@ -13,19 +13,15 @@ export default function ManageData() {
             </div>
             <div className="middleContainer">
                 <div className="buttonContainer">
-                    <button className="button" type="submit">Connected Platforms</button>
+                    <button className="button" type="submit">View My Profile Details</button>
                     <i className="fa-solid fa-chevron-right"></i>
                 </div>
                 <div className="buttonContainer">
-                    <button className="button" type="submit">View Token Details</button>
+                    <button className="button" type="submit">Data Permission</button>
                     <i className="fa-solid fa-chevron-right"></i>
                 </div>
                 <div className="buttonContainer">
-                    <button className="button" type="submit">My Documents</button>
-                    <i className="fa-solid fa-chevron-right"></i>
-                </div>
-                <div className="buttonContainer">
-                    <button className="button" type="submit">Manage My Data</button>
+                    <button className="button" type="submit">Delete My Data</button>
                     <i className="fa-solid fa-chevron-right"></i>
                 </div>
             </div>
